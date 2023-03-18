04e1825cbee3f40da976489e9cf1d344f047cec7
John Jackson <mrfrenik@gmail.com>
2023-03-18 10:51:43 -0500

    update unity template

--- source/proj_gen/templates/editor/unity.c
+++ source/proj_gen/templates/editor/unity.c
@@ -1,7 +1,7 @@
 
 // Core Includes
 #define GS_CORE_EDITOR_IMPL
-#include %EDITOR_GENERATEED_PATH%
+#include %EDITOR_GENERATEED_SOURCE_PATH%
 #include <editor/gs_core_editor.c>
 #include <core/gs_core_app.c>
 
